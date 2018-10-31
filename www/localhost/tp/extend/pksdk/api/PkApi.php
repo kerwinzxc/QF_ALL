f771770aaa7f4df22f60ed81996c2a900ffad25e
Mario <mario.sway2019@gmail.com>
2018-10-31 00:33:55 -0400

    Add purchases and consume api

--- www/localhost/tp/extend/pksdk/api/PkApi.php
+++ www/localhost/tp/extend/pksdk/api/PkApi.php
@@ -8,6 +8,7 @@ namespace pksdk\api;
 use think\Log;
 
 define("PK_API_ROOT", "https://playground.dobby.sandbx.co/");
+define("PK_GAME_UUID", "demo-game");
 
 class PkApi {
 
@@ -35,6 +36,44 @@ class PkApi {
     public static function getUser($accessToken){
         $apiUri = PK_API_ROOT . "api/portal/api/user";
         $rs = \pksdk\request\Request::getWithPkTokenJsonResponse($apiUri, $accessToken);
+        return $rs;
+    }
+
+    /*
+     *        {
+     *           "state": "PURCHASED",
+     *           "purchaseUuid": "53ed5476-98a8-4e08-83e6-41733ed77b27",
+     *           "type": "ASSET",
+     *           "loginEmail": "demo@example.com",
+     *           "itemUuid": "demo-game-gold10",
+     *           "gameUuid": "demo-game",
+     *           "price": 10,
+     *           "purpose": "Demo Game, Gold 10"
+     *       },
+     */
+    public static function listPurchaseItems($accessToken){
+        $apiUri = PK_API_ROOT . "api/games/" . PK_GAME_UUID . "/purchases";
+        $rs = \pksdk\request\Request::getWithPkTokenJsonResponse($apiUri, $accessToken);
+        return $rs;
+    }
+
+    /*
+     * {
+     *   "status": "SUCCESS",
+     *   "purchaseUuid": "53ed5476-98a8-4e08-83e6-41733ed77b27",
+     *   "type": "ASSET",
+     *   "loginEmail": "demo@example.com",
+     *   "itemUuid": "demo-game-gold10",
+     *   "gameUuid": "demo-game"
+     *  }
+     */
+
+    public static function consumeItem($purchaseUuid, $accessToken){
+        $apiUri = PK_API_ROOT . "api/purchases/{$purchaseUuid}/consume";
+        $data = array(
+            'purchaseUuid' => $purchaseUuid,
+        );
+        $rs = \pksdk\request\Request::postWithPkTokenJsonResponse($apiUri, $data, $accessToken);
         return $rs;
     }
 }
